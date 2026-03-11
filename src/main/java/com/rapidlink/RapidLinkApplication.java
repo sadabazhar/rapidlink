291db363e947f80a54e7f59200c5226a6a9c0955
SADAB AZHAR <sadabazhar84@gmail.com>
2026-03-11 14:48:11 +0530

    Initial commit - Spring Boot RapidLink project setup

---
+++ src/main/java/com/rapidlink/RapidLinkApplication.java
@@ -0,0 +1,13 @@
+package com.rapidlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rapidlink/RapidLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapidlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
