3cc0002bb91d66f4be2a5d4cb360e8fa64177770
SADAB AZHAR <sadabazhar84@gmail.com>
2026-03-15 12:24:39 +0530

    feat: add PostgreSQL docker setup, Flyway config, and HikariCP tuning

--- src/main/java/com/rapidlink/RapidLinkApplication.java
+++ src/main/java/com/rapidlink/RapidLinkApplication.java
@@ -3,11 +3,16 @@ package com.rapidlink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class RapidLinkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RapidLinkApplication.class, args);
+
+        // Set timezone at JVM level
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+        SpringApplication.run(RapidLinkApplication.class, args);
 	}
 
 }
